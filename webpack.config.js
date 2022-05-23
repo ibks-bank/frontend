9a2c411332d8208230dc64a57c4f43714a82e22a
Roman Roshchin <123rosrom@gmail.com>
2022-05-24 00:00:52 +0300

    frontend: fix CSP

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,15 @@ module.exports = {
         template: './src/index.html'
     })],
     devServer: {
-        historyApiFallback: true
+        historyApiFallback: true,
+        headers: {
+            "X-Frame-Options" : "DENY",
+            "Content-Security-Policy" : "default-src 'self' bank.sytes.net:3011 bank.sytes.net:3001 *.bootstrapcdn.com 'unsafe-eval' data:; frame-src 'self'; frame-ancestors 'self' bank.sytes.net:3011 bank.sytes.net:3001; form-action 'self' bank.sytes.net:3011 bank.sytes.net:3001; style-src 'self' https://netdna.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css 'unsafe-inline';",
+            "Cache-Control" : "Cache-Control: no-cache, no-store, must-revalidate",
+            "X-XSS-Protection" : "0",
+            "X-Content-Type-Options" : "nosniff"
+        }
+
     },
     externals: {
         // global app config object
